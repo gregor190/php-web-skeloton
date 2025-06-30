fa7bad25d26d7c64c8e798026949fa002a8ed791
gregor190 <gregor.damdam@gmail.com>
2025-06-30 13:10:58 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+echo '<h1>Welcome to Gregor’s Dev Server</h1>';
+echo '<a href="status.php"><button>Status Page</button type="button"></a>';
+?>
---
+++ status.php
@@ -0,0 +1,6 @@
+<?php
+echo '<h1>Welcome to Gregor’s status page</h1>';
+echo '<a href="index.php"><button>Home</button type="button"></a>';
+phpinfo();
+?>
+
